5d1439309f5500f26a0a2235d0f23d8e4c9efb5a
vinodkottem <vinodkottem@gmail.com>
2017-03-14 20:31:17 +0530

    okay

--- server/boot/appendcodes.js
+++ server/boot/appendcodes.js
@@ -2,7 +2,7 @@ module.exports = function(server) {
 	var remotes = server.remotes();
 	var app = require('../server');
 	remotes.before('**', function (ctx, next, method) {
-		console.log("1. EXPECTATION: This before remote in SERVER/BOOT gets called first");
+		console.log("1. EXPECTATION: This remotes.before hook in SERVER/BOOT to be called first");
 		next();
 	});
 };
